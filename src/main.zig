d62564bd8398d0665277dcac7caec81cc12c7970
abrasumente <345396594@qq.com>
2023-04-13 13:31:53 +0800

    interpret at compile-time, hard-coded input, boring stuff

--- src/main.zig
+++ src/main.zig
@@ -2,21 +2,22 @@ const std = @import("std");
 
 const memorySize = 1024 * 1024; // 1 MiB
 
-var memory = [_]u8{0} ** memorySize;
+fn fuck() void {
+    comptime var memory = [_]u8{0} ** memorySize;
 
-pub fn main() !void {
     // Adding memory[0] and memory[1] and store the result in memory[1]
     const prog = "[->+<]";
-    memory[0] = 1;
-    memory[1] = 2;
+    memory[0] = 44;
+    memory[1] = 42;
     std.debug.print("adding {} and {}\n", .{ memory[0], memory[1] });
 
     // Interpret the program
     const stdin = std.io.getStdIn().reader();
-    var ip: u32 = 0; // instruction pointer
-    var dp: u32 = 0; // data pointer
+    comptime var ip: u32 = 0; // instruction pointer
+    comptime var dp: u32 = 0; // data pointer
 
-    while (ip != prog.len) {
+    inline while (ip != prog.len) {
+        @setEvalBranchQuota(1000000);
         const c = prog[ip];
         switch (c) {
             '>' => dp += 1,
@@ -26,8 +27,8 @@ pub fn main() !void {
             '.' => std.debug.print("{}", .{memory[dp]}),
             ',' => memory[dp] = try stdin.readByte(),
             '[' => if (memory[dp] == 0) {
-                var depth: u32 = 1;
-                while (depth != 0) {
+                comptime var depth: u32 = 1;
+                inline while (depth != 0) {
                     ip += 1;
                     const c0 = prog[ip];
                     if (c0 == '[') {
@@ -38,8 +39,8 @@ pub fn main() !void {
                 }
             },
             ']' => if (memory[dp] != 0) {
-                var depth: u32 = 1;
-                while (depth != 0) {
+                comptime var depth: u32 = 1;
+                inline while (depth != 0) {
                     ip -= 1;
                     const c0 = prog[ip];
                     if (c0 == ']') {
@@ -54,5 +55,11 @@ pub fn main() !void {
         ip += 1;
     }
 
+    comptime try std.testing.expect(memory[1] == 86);
+
     std.debug.print("result: {}\n", .{memory[1]});
+}
+
+pub fn main() !void {
+    fuck();
 }
